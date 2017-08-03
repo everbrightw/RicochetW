e9bd5de06bd4c2b46510be29f61b78a366c35a8d
everbrightw <yusencs@163.com>
2017-08-03 16:05:36 -0400

    initialize buttons and add first map

--- app/src/main/java/com/everbright/wangyusen/ricochetw/MainActivity.java
+++ app/src/main/java/com/everbright/wangyusen/ricochetw/MainActivity.java
@@ -3,11 +3,32 @@ package com.everbright.wangyusen.ricochetw;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import info.hoang8f.widget.FButton;
+
 public class MainActivity extends AppCompatActivity {
+    private FButton moveLeft;
+    private FButton moveRight;
+    private FButton moveUp;
+    private FButton moveDown;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeMoveButtons();
+    }
+
+    /**
+     * initialize left right up down buttons
+     */
+    public void initializeMoveButtons(){
+        moveRight = (FButton) findViewById(R.id.right_button);
+        moveDown = (FButton) findViewById(R.id.down_button);
+        moveUp = (FButton) findViewById(R.id.up_button);
+        moveLeft = (FButton) findViewById(R.id.left_button);
+        moveLeft.setText("left");
+        moveRight.setText("right");
+        moveUp.setText("up");
+        moveDown.setText("down");
     }
 }
